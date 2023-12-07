64bfd7dddf8380f58c04fb30dd41a702fa933d2d
Nick2022 <876927113@qq.com>
2023-12-07 11:39:01 +0800

    Revert "init kmm compose project."
    This reverts commit cf4959a290eb85114da33a55173ef65b4b11c956.

--- shared/src/commonMain/kotlin/com/hele/compose_kmm/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.hele.compose_kmm
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/hele/compose_kmm/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.hele.compose_kmm
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
